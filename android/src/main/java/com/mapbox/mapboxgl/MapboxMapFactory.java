f6c2d91679ae1212527959d7adc2a6a82aa7e7ef
guangxian.w <zouguangxian@hyn.space>
2019-12-26 14:56:21 +0800

    Fixed map plugin error when multiple maps

--- android/src/main/java/com/mapbox/mapboxgl/MapboxMapFactory.java
+++ android/src/main/java/com/mapbox/mapboxgl/MapboxMapFactory.java
@@ -19,30 +19,30 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class MapboxMapFactory extends PlatformViewFactory {
 
-  private final AtomicInteger mActivityState;
-  private final Registrar mPluginRegistrar;
+    private final AtomicInteger mActivityState;
+    private final Registrar mPluginRegistrar;
 
-  public MapboxMapFactory(AtomicInteger state, Registrar registrar) {
-    super(StandardMessageCodec.INSTANCE);
-    mActivityState = state;
-    mPluginRegistrar = registrar;
-  }
-
-  @Override
-  public PlatformView create(Context context, int id, Object args) {
-    Log.i("MapboxMapFactory", "create PlatformView " + id);
-    Map<String, Object> params = (Map<String, Object>) args;
-    final MapboxMapBuilder builder = new MapboxMapBuilder();
-
-    Convert.interpretMapboxMapOptions(params.get("options"), builder);
-    if (params.containsKey("initialCameraPosition")) {
-      CameraPosition position = Convert.toCameraPosition(params.get("initialCameraPosition"));
-      builder.setInitialCameraPosition(position);
+    public MapboxMapFactory(AtomicInteger state, Registrar registrar) {
+        super(StandardMessageCodec.INSTANCE);
+        mActivityState = state;
+        mPluginRegistrar = registrar;
     }
-    //register plugins
-    MapPluginsManager.INSTANCE.registerBuilder(new HeavenMapBuilder().interpretOptions(params.get("plugins-options")));
-    MapPluginsManager.INSTANCE.registerBuilder(new MapRouteBuilder().interpretOptions(params.get("plugins-options")));
-    MapPluginsManager.INSTANCE.buildPlugins();
-    return builder.build(id, context, mActivityState, mPluginRegistrar);
-  }
+
+    @Override
+    public PlatformView create(Context context, int id, Object args) {
+        Log.i("MapboxMapFactory", "create PlatformView " + id);
+        Map<String, Object> params = (Map<String, Object>) args;
+        final MapboxMapBuilder builder = new MapboxMapBuilder();
+
+        Convert.interpretMapboxMapOptions(params.get("options"), builder);
+        if (params.containsKey("initialCameraPosition")) {
+            CameraPosition position = Convert.toCameraPosition(params.get("initialCameraPosition"));
+            builder.setInitialCameraPosition(position);
+        }
+        //register plugins
+        MapPluginsManager.INSTANCE.registerBuilder(id, new HeavenMapBuilder().interpretOptions(params.get("plugins-options")));
+        MapPluginsManager.INSTANCE.registerBuilder(id, new MapRouteBuilder().interpretOptions(params.get("plugins-options")));
+        MapPluginsManager.INSTANCE.buildPlugins(id);
+        return builder.build(id, context, mActivityState, mPluginRegistrar);
+    }
 }
